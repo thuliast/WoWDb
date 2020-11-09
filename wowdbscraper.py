c69dcf6a7934cb614cba5ea569fa1511dc052bbf
thuliast <francesco.tattoli@gmail.com>
2020-11-09 12:18:01 +0100

    Release 0.5b. Results on table format and choice of export on html file

--- wowdbscraper.py
+++ wowdbscraper.py
@@ -6,7 +6,7 @@ Author: thuliast
 Creation Date: 18-12-2019
 """
 
-VERSION = "0.5a"
+VERSION = "0.5b"
 import sqlite3
 import sys
 from prettytable import from_db_cursor
@@ -18,7 +18,7 @@ print("|                                        |")
 print("|           World of Warcraft            |")
 print("|               Item List                |")
 print("|                                        |")
-print("|              Version:",VERSION,"             |")
+print("|              Version:",VERSION,"            |")
 print("|                                        |")
 print("------------------------------------------\n")
 
@@ -39,12 +39,23 @@ class encyclopedia(object):
         self.item_to_search = item_to_search
         self.cur = self.con.cursor()
         self.cur.execute("SELECT entry, name, itemlevel, requiredlevel \
-FROM item_list WHERE name LIKE ? ORDER BY entry",('%{}%'.format(self.item_to_search),))        
+FROM item_list WHERE name LIKE ? ORDER BY entry",('%{}%'.format(self.item_to_search),))
+
+        #Using prettytable to display results with fashion
         self.tb = from_db_cursor(self.cur)
+        self.tb.sortby = "name"
         print(self.tb.get_string(title="List of Items found"))
         
-        print("-" * 80)
-        self.choice = input("Another search(y) or quit(n)? :")
+        #Give the choice to export results for future reference
+        self.export_choice = input("Export results in HTML? (y/n):")
+        self.export_choice = self.export_choice.lower()
+        if self.export_choice == "y":
+            with open("results.html","w") as f:
+                f.write(self.tb.get_html_string())
+                print("File 'results.html' saved in the Project Folder")
+        
+        #Give the choice to start another search
+        self.choice = input("Another search or quit (y/n)? :")
         self.choice = self.choice.lower()
         if self.choice == "n":
             print("Have a nice day")
